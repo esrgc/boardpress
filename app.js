f2c6779eb35c05687461fb1ba41a8d90dcdcf537
Frank Rowe <fsrowe@gmail.com>
2014-02-05 15:27:20 -0500

    remove users

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
   , api = require('./routes/api')
@@ -31,7 +30,6 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index)
-app.get('/users', user.list)
 app.get('/getRoutes', api.getRoutes)
 
 http.createServer(app).listen(app.get('port'), function(){
