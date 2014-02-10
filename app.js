92139367cb72f793900fec820178e185d921ece1
Frank Rowe <fsrowe@gmail.com>
2014-02-10 16:20:09 -0500

    additional queries

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000)
   app.set('views', __dirname + '/views')
   app.set('view engine', 'ejs')
-  app.use(express.favicon())
   app.use(express.logger('dev'))
   app.use(express.bodyParser())
   app.use(express.methodOverride())
@@ -32,6 +31,8 @@ app.get('/', routes.index)
 app.get('/getRoutes', api.getRoutes)
 app.get('/getStops', api.getStops)
 app.get('/getStopsMap', api.getStopsMap)
+app.get('/getShifts', api.getShifts)
+app.get('/getTrips', api.getTrips)
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'))
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,36 @@
 var db = require('../db')
 
 exports.getRoutes = function(req, res){
-  var statement = 'select route_refid, route_shortname, route_color, route_estmileage from routes'
-  db.executeStatement(statement, function(routes) {
-    res.json(routes)
+  var statement = 'select route_refid as id, route_shortname as name, route_color as color, route_estmileage as mileage from routes'
+  db.executeStatement(statement, function(data) {
+    res.json(data)
+  })
+}
+
+exports.getStops = function(req, res){
+  var statement = 'select stop_refid as id, stop_name as name, stop_status as status from stops'
+  db.executeStatement(statement, function(data) {
+    res.json(data)
+  })
+}
+
+exports.getStopsMap = function(req, res){
+  var statement = 'select stop_refid as id, stop_name as name, stop_lat as lat, stop_lon as lng from stops'
+  db.executeStatement(statement, function(data) {
+    res.json(data)
+  })
+}
+
+exports.getShifts = function(req, res){
+  var statement = 'select shift_refid as id, shift_name as name, shift_startdeparttime as depart, shift_endarrivaltime as arrival from shifts'
+  db.executeStatement(statement, function(data) {
+    res.json(data)
+  })
+}
+
+exports.getTrips = function(req, res){
+  var statement = 'select trip_refid as id, trip_startdeparttime as depart, trip_endarrivaltime as arrival from trips where tripinservice_id = 1'
+  db.executeStatement(statement, function(data) {
+    res.json(data)
   })
 }
