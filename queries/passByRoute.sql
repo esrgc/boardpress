ed02a79a57808debb3ecb2ea21346e140d12fc33
bdobel <bedobelstein@salisbury.edu>
2014-02-11 12:19:15 -0500

    Extend ISNULL to all cnts. Add filter examples.

--- queries/passByRoute.sql
+++ queries/passByRoute.sql
@@ -1,5 +1,6 @@
-select [Name],[CHILD],[DIS],[ELD],[EMP],[PCA],[RF],[STP],[TCA],[VET]
-,(isnull([CHILD],0)+isnull([DIS],0)+isnull([ELD],0)+isnull([EMP],0)+isnull([PCA],0)+isnull([RF],0)+isnull([STP],0)+isnull([TCA],0)+isnull([VET],0)) as [On]
+select [Name], isnull([CHILD],0) as [CHILD], isnull([DIS],0) as [DIS], isnull([ELD],0) as [ELD], isnull([EMP],0) as [EMP]
+, isnull([PCA],0) as [PCA], isnull([RF],0) as [RF], isnull([STP],0) as [STP], isnull([TCA],0) as [TCA], isnull([VET],0) as [VET]
+, (isnull([CHILD],0)+isnull([DIS],0)+isnull([ELD],0)+isnull([EMP],0)+isnull([PCA],0)+isnull([RF],0)+isnull([STP],0)+isnull([TCA],0)+isnull([VET],0)) as [On]
 ,[Off]
 from 
 (select a.route_refid as [Name], passType_refid as [Rider], sum(b.passenger_on_count) as [Cnt]
@@ -7,14 +8,16 @@ from [STRoute].[dbo].[passengers_bystop] a
 left join [STRoute].[dbo].[passengers_bystopdetails] b
 on a.pbs_id = b.pbs_id
 where passType_refid is not null
---and stopdate = '2013-06-01'
 --{{{filters}}}
+--and stopdate between '2013-06-01' and '2013-06-30' (for date range) 
+--and datepart(weekday,stopdate) in ('2','3','4','5','6') (for specific day of week; where sunday = 1 and saturday = 7)
+--and passType_refid = 'DIS' (for specific passenger type)
 group by a.route_refid, passType_refid
 ) as magicTable
 pivot
 (SUM([Cnt]) for [Rider] in ([CHILD],[DIS],[ELD],[EMP],[PCA],[RF],[STP],[TCA],[VET],[On])) as pvt
-left join (select route_refid, sum([PassengersOff]) [Off] from [STRoute].[dbo].[passengers_bystop] 
---where {{{filters}}} 
+left join (select route_refid, sum([PassengersOff]) [Off] from [STRoute].[dbo].[passengers_bystop]
+--where {{{filters}}}
 group by route_refid) as a
 on [Name] = a.route_refid
 order by [Name]
