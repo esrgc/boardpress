6ed9e3f51ac36c25ff4d67b1424aabfb75ef5a87
Frank Rowe <fsrowe@gmail.com>
2014-02-12 16:29:46 -0500

    port 3006

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var express = require('express')
 var app = express()
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000)
+  app.set('port', process.env.PORT || 3006)
   app.set('views', __dirname + '/views')
   app.set('view engine', 'ejs')
   app.use(express.logger('dev'))
