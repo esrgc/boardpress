6208da59d0c61de3e9e76cdc1c71e38eac559f2a
bdobel <bedobelstein@salisbury.edu>
2014-02-13 14:10:21 -0500

    Broke out filters and variable for drill-down.

--- queries/fareByShift.sql
+++ queries/fareByShift.sql
@@ -1,47 +1,46 @@
---REQUEST ST:  Breakout on how much fare should have been collected for a particular shift on a particular day
-select [Name], [Date], sum([Fare]) as [Fare]
-from (select c.[shift_name] as [Name], cast(shift_date as date) as [Date], datepart(weekday,a.stopdate) as [Day], [Fare]
-from [STRoute].[dbo].[passengers_bystop] a
-join (select pbs_id, sum(gross_passenger_fare) as [Fare]
-from [STRoute].[dbo].[passengers_bystopdetails]
-group by pbs_id) b
-on a.pbs_id = b.pbs_id
-join [STRoute].[dbo].[shifts] c
-on a.shift_refid = c.shift_refid) a
---where {{{filters}}} ex:[Day] = 1 or [Date] between X and Y
-group by [Name], [Date]
-order by [Name], [Date]
-
-------------------------------------Results displayed as pivot table------------------------------------
+--where {{{[variable]}}} can be [Route],[Shift],[Trip],[Stop]
 declare @cols as nvarchar(max),
 	@query  as nvarchar(max)
 
 set @cols = stuff((select ',' + quotename([Date])
-            from (select distinct cast(shift_date as date) as [Date], datepart(weekday, stopdate) as [Day]
-			from [STRoute].[dbo].[passengers_bystop]) z
-			--where {{{filters}}} ex:[Date] between X and Y or [Day] = 1
+            from (select distinct cast(stopdate as date) as [Date]
+			from [STRoute].[dbo].[passengers_bystop]
+			where stopdate is not null
+			{{{dateRangeFilter}}}
+			{{{dayFilter}}}
+			) z
             order by [Date]
 		    for xml path(''))--, type
            -- ).value('.', 'nvarchar(max)') 
         ,1,1,'')
 		
 set @query = 
-'select [Name], ' + @cols + ' 
+'select {{{[variable]}}}, ' + @cols + ' 
 from 
-(select [Name], [Date], sum([Fare]) as [Fare]
-from (select c.[shift_name] as [Name], cast(shift_date as date) as [Date], datepart(weekday,a.stopdate) as [Day], [Fare]
+(select {{{[variable]}}}, [Date], sum([Fare]) as [Fare]
+from (select a.[route_refid] as [Route], a.[shift_refid] as [Shift], [trip_refid] as [Trip]
+, [stop_refid] as [Stop], cast(a.stopdate as date) as [Date], [Fare]
 from [STRoute].[dbo].[passengers_bystop] a
 join (select pbs_id, sum(gross_passenger_fare) as [Fare]
 from [STRoute].[dbo].[passengers_bystopdetails]
+where pbs_id is not null
+{{{passFilter}}}
 group by pbs_id) b
 on a.pbs_id = b.pbs_id
 join [STRoute].[dbo].[shifts] c
-on a.shift_refid = c.shift_refid) a
---where {{{filters}}} ex:[Name] = ''111a, 111r (Shift 1)''
-group by [Name], [Date]
+on a.shift_refid = c.shift_refid
+Where [route_refid] is not null
+{{{dateRangeFilter}}}
+{{{dayFilter}}}
+{{{routeFilter}}}
+{{{shiftFilter}}}
+{{{tripFilter}}}
+{{{stopFilter}}}
+) a
+group by {{{[variable]}}}, [Date]
 ) as magicTable
 pivot
 (sum([Fare]) for [Date] in (' + @cols + ')) as pvt
-order by [Name]'
+order by {{{[variable]}}}'
 
 execute(@query)
