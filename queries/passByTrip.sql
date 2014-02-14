73306179e42a1087c2644f0cca7f58219e2f097b
bdobel <bedobelstein@salisbury.edu>
2014-02-14 15:26:06 -0500

    removed route_refid from d

--- queries/passByTrip.sql
+++ queries/passByTrip.sql
@@ -3,12 +3,12 @@ select pvt.[Name], isnull([CHILD],0) as [CHILD], isnull([DIS],0) as [DIS], isnul
 , (isnull([CHILD],0)+isnull([DIS],0)+isnull([ELD],0)+isnull([EMP],0)+isnull([PCA],0)+isnull([RF],0)+isnull([STP],0)+isnull([TCA],0)+isnull([VET],0)) as [On]
 , isnull([Off],0) as [Off]
 from 
-(select d.route_refid + '  ('+isnull(substring(cast(d.trip_startdeparttime as varchar),1,5), '')+ ')' as [Name]
+(select route_refid + '  ('+isnull(substring(cast(d.trip_startdeparttime as varchar),1,5), '')+ ')' as [Name]
 , passType_refid as [Rider], sum(b.passenger_on_count) as [Cnt]
 from [STRoute].[dbo].[passengers_bystop] a
 join [STRoute].[dbo].[passengers_bystopdetails] b
 on a.pbs_id = b.pbs_id
-join [STRoute].[dbo].[trips] d
+join (select trip_refid, trip_startdeparttime from [STRoute].[dbo].[trips]) d
 on a.trip_refid = d.trip_refid
 where passType_refid is not null
 {{{dateRangeFilter}}}
@@ -18,22 +18,22 @@ where passType_refid is not null
 {{{shiftFilter}}}
 {{{tripFilter}}}
 {{{stopFilter}}}
-group by d.route_refid + '  ('+isnull(substring(cast(d.trip_startdeparttime as varchar),1,5), '')+ ')', passType_refid
+group by route_refid + '  ('+isnull(substring(cast(d.trip_startdeparttime as varchar),1,5), '')+ ')', passType_refid
 ) as magicTable
 pivot
 (SUM([Cnt]) for [Rider] in ([CHILD],[DIS],[ELD],[EMP],[PCA],[RF],[STP],[TCA],[VET],[On])) as pvt
-left join (select d.route_refid + '  ('+isnull(substring(cast(d.trip_startdeparttime as varchar),1,5), '')+ ')' as [Name]
+left join (select route_refid + '  ('+isnull(substring(cast(d.trip_startdeparttime as varchar),1,5), '')+ ')' as [Name]
 , sum([PassengersOff]) as [Off]
 from [STRoute].[dbo].[passengers_bystop] a
-left join [STRoute].[dbo].[trips] d
+left join (select trip_refid, trip_startdeparttime from [STRoute].[dbo].[trips]) d
 on a.trip_refid = d.trip_refid
-where d.route_refid is not null
+where route_refid is not null
 {{{dateRangeFilter}}}
 {{{dayFilter}}}
 {{{routeFilter}}}
 {{{shiftFilter}}}
 {{{tripFilter}}}
 {{{stopFilter}}}
-group by d.route_refid + '  ('+isnull(substring(cast(d.trip_startdeparttime as varchar),1,5), '')+ ')') as a
+group by route_refid + '  ('+isnull(substring(cast(d.trip_startdeparttime as varchar),1,5), '')+ ')') as a
 on pvt.[Name] = a.[Name]
 order by pvt.[name]
