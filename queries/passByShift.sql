04e84b209634a30389609c75aa8a77f0f5f094b5
bdobel <bedobelstein@salisbury.edu>
2014-02-17 11:40:53 -0500

    select [ID],[Short],[Long] and table now a

--- queries/passByShift.sql
+++ queries/passByShift.sql
@@ -1,15 +1,15 @@
-select [Name], isnull([CHILD],0) as [CHILD], isnull([DIS],0) as [DIS], isnull([ELD],0) as [ELD], isnull([EMP],0) as [EMP]
+sselect [ID], [Short], [Long], isnull([CHILD],0) as [CHILD], isnull([DIS],0) as [DIS], isnull([ELD],0) as [ELD], isnull([EMP],0) as [EMP]
 , isnull([PCA],0) as [PCA], isnull([RF],0) as [RF], isnull([STP],0) as [STP], isnull([TCA],0) as [TCA], isnull([VET],0) as [VET]
 , (isnull([CHILD],0)+isnull([DIS],0)+isnull([ELD],0)+isnull([EMP],0)+isnull([PCA],0)+isnull([RF],0)+isnull([STP],0)+isnull([TCA],0)+isnull([VET],0)) as [On]
 , isnull([Off],0) as [Off]
 from 
-(select c.shift_name as [Name], passType_refid as [Rider], sum(b.passenger_on_count) as [Cnt]
+(select a.shift_refid as [ID], c.shift_name as [Short], c.[shift_description] as [Long], b.passType_refid as [Rider], sum(b.passenger_on_count) as [Cnt]
 from [STRoute].[dbo].[passengers_bystop] a
 left join [STRoute].[dbo].[passengers_bystopdetails] b
 on a.pbs_id = b.pbs_id
 left join [STRoute].[dbo].[shifts] c
 on a.shift_refid = c.shift_refid
-where passType_refid is not null
+where b.passType_refid is not null
 {{{dateRangeFilter}}}
 {{{dayFilter}}}
 {{{passFilter}}}
@@ -17,18 +17,18 @@ where passType_refid is not null
 {{{shiftFilter}}}
 {{{tripFilter}}}
 {{{stopFilter}}}
-group by c.shift_name, passType_refid
+group by a.shift_refid, c.shift_name, c.[shift_description], b.passType_refid
 ) as magicTable
 pivot
 (SUM([Cnt]) for [Rider] in ([CHILD],[DIS],[ELD],[EMP],[PCA],[RF],[STP],[TCA],[VET],[TOTAL])) as pvt
-left join (select shift_name, sum([PassengersOff]) [Off] from [STRoute].[dbo].[passengers_bystop] a left join [STRoute].[dbo].[shifts] c on a.shift_refid = c.shift_refid
-where shift_name is not null
+left join (select a.shift_refid, sum([PassengersOff]) [Off] from [STRoute].[dbo].[passengers_bystop] a left join [STRoute].[dbo].[shifts] c on a.shift_refid = c.shift_refid
+where c.shift_name is not null
 {{{dateRangeFilter}}}
 {{{dayFilter}}}
 {{{routeFilter}}}
 {{{shiftFilter}}}
 {{{tripFilter}}}
 {{{stopFilter}}}
-group by shift_name) as a
-on [Name] = a.shift_name
-order by [Name]
+group by a.shift_refid) as a
+on [ID] = a.shift_refid
+order by [Short]
