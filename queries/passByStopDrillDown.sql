764cfd372d04101daa9c5358135dafc63f1f179b
bdobel <bedobelstein@salisbury.edu>
2014-02-17 14:08:53 -0500

    select [ID],[Short] there is no [Long]. Table a

--- queries/passByStopDrillDown.sql
+++ queries/passByStopDrillDown.sql
@@ -1,13 +1,15 @@
-select [Name], isnull([CHILD],0) as [CHILD], isnull([DIS],0) as [DIS], isnull([ELD],0) as [ELD], isnull([EMP],0) as [EMP]
+select [ID], [Short], isnull([CHILD],0) as [CHILD], isnull([DIS],0) as [DIS], isnull([ELD],0) as [ELD], isnull([EMP],0) as [EMP]
 , isnull([PCA],0) as [PCA], isnull([RF],0) as [RF], isnull([STP],0) as [STP], isnull([TCA],0) as [TCA], isnull([VET],0) as [VET]
 , (isnull([CHILD],0)+isnull([DIS],0)+isnull([ELD],0)+isnull([EMP],0)+isnull([PCA],0)+isnull([RF],0)+isnull([STP],0)+isnull([TCA],0)+isnull([VET],0)) as [On]
 , isnull([Off],0) as [Off]
 from
-(select stop_refid as [Name], stop_sequence as [Order], passType_refid as [Rider], sum(b.passenger_on_count) as [Cnt]
+(select a.stop_refid as [ID], c.[stop_name] as [Short], a.stop_sequence as [Order], passType_refid as [Rider], sum(b.passenger_on_count) as [Cnt]
 from [STRoute].[dbo].[passengers_bystop] a
 full join [STRoute].[dbo].[passengers_bystopdetails] b
 on a.pbs_id = b.pbs_id
-where stop_refid is not null
+left join [STRoute].[dbo].[stops] c
+on a.stop_refid = c.stop_refid
+where a.stop_refid is not null
 {{{dateRangeFilter}}}
 {{{dayFilter}}}
 {{{passFilter}}}
@@ -15,19 +17,19 @@ where stop_refid is not null
 {{{shiftFilter}}}
 {{{tripFilter}}}
 {{{stopFilter}}}
-group by stop_refid, stop_sequence, passType_refid
+group by a.stop_refid, c.[stop_name], a.stop_sequence, b.passType_refid
 ) as magicTable
 pivot
 (SUM([Cnt]) for [Rider] in ([CHILD],[DIS],[ELD],[EMP],[PCA],[RF],[STP],[TCA],[VET],[On])) as pvt
-full join (select stop_refid, stop_sequence, sum([PassengersOff]) as [Off] from [STRoute].[dbo].[passengers_bystop]
-where stop_refid is not null
+full join (select a.stop_refid, a.stop_sequence, sum(a.[PassengersOff]) as [Off] from [STRoute].[dbo].[passengers_bystop] a
+where a.stop_refid is not null
 {{{dateRangeFilter}}}
 {{{dayFilter}}}
 {{{routeFilter}}}
 {{{shiftFilter}}}
 {{{tripFilter}}}
 {{{stopFilter}}}
-group by stop_refid, stop_sequence) as a
-on [Name] = a.stop_refid
+group by a.stop_refid, a.stop_sequence) as a
+on [ID] = a.stop_refid
 and [Order] = a.stop_sequence
 order by [Order]
