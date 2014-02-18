2be01b4d3cb2a428827f0269645e0fd6feb8703a
bdobel <bedobelstein@salisbury.edu>
2014-02-18 15:57:26 -0500

    Added [Name]

--- queries/passByRoute.sql
+++ queries/passByRoute.sql
@@ -1,9 +1,9 @@
-select [ID], [Short], [Long], isnull([CHILD],0) as [CHILD], isnull([DIS],0) as [DIS], isnull([ELD],0) as [ELD], isnull([EMP],0) as [EMP]
+select [ID], [Name], [Short], [Long], isnull([CHILD],0) as [CHILD], isnull([DIS],0) as [DIS], isnull([ELD],0) as [ELD], isnull([EMP],0) as [EMP]
 , isnull([PCA],0) as [PCA], isnull([RF],0) as [RF], isnull([STP],0) as [STP], isnull([TCA],0) as [TCA], isnull([VET],0) as [VET]
 , (isnull([CHILD],0)+isnull([DIS],0)+isnull([ELD],0)+isnull([EMP],0)+isnull([PCA],0)+isnull([RF],0)+isnull([STP],0)+isnull([TCA],0)+isnull([VET],0)) as [On]
 ,isnull([Off],0) as [Off]
 from 
-(select a.route_refid as [ID], c.[route_shortname] as [Short], c.[route_longname] as [Long], b.passType_refid as [Rider], sum(b.passenger_on_count) as [Cnt]
+(select a.route_refid as [ID], a.route_refid as [Name], c.[route_shortname] as [Short], c.[route_longname] as [Long], b.passType_refid as [Rider], sum(b.passenger_on_count) as [Cnt]
 from [STRoute].[dbo].[passengers_bystop] a
 left join [STRoute].[dbo].[passengers_bystopdetails] b
 on a.pbs_id = b.pbs_id
